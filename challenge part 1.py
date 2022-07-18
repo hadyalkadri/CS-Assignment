c805e3510c851790abbe3a75998a75c0657262bd
hadyalkadri <98332279+hadyalkadri@users.noreply.github.com>
2022-07-18 18:54:25 +0300

    Add files via upload

---
+++ challenge part 1.py
@@ -0,0 +1,60 @@
+# #level 1
+# print("Program to to check whether it is a leap year or not.")
+# print("-----------------------------------------------------")
+# year = int(input("Enter a year to test: "))
+#
+#
+# if (((year % 400 == 0) or (year % 100 != 0)) and (year % 4 == 0)):
+#     print("{0} is a leap year".format(year))
+# else:
+#     print("{0} is not a leap year".format(year))
+# print("\n")
+# print("Have a good day :)")
+
+#Level 2
+# for i in range(1,11):
+#         print(*("{:3}".format(i*j)  for j in range(1, 11)))
+
+            # x = i, i*j
+       # print(x, '\t')
+
+
+#Level 3
+# def perfect_no():
+#     n = int(input("Enter a number to check whether perfect or not: "))
+#     sum = 0
+#     for i in range(1,n):
+#         if n % i == 0:
+#             sum += i
+#     if sum == n:
+#         return('Number is perfect.')
+#     else:
+#         return('Number is not perfect.')
+#
+# print(perfect_no())
+
+#Level 4
+
+# def reverse():
+#     name = input("Enter any string: ")
+#     temp= ""
+#
+#     for i in name:
+#         temp = i + temp
+#
+#     return(temp)
+# print(reverse())
+
+#Level 6
+
+# List = ["hady"]
+# List = len(List)
+#
+# temp_list = []
+#
+# for i in range(len(List)):
+#     if List[i]/2 != 0:
+#         temp_list.append(i)
+#     print(temp_list)
+
+
